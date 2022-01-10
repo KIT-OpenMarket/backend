9100dbf58506fc53d73f600124b751cfd60f6c04
khj745700 <khj745700@naver.com>
2022-01-10 21:18:50 +0900

    몽고디비 연결 및 엔티티 PIN등록

---
+++ src/domain/entity/pin.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const {Schema } = mongoose;
+
+const pinSchema = new Schema ( {
+    latitude : {
+        type : Number,
+        required : true,
+    },
+    longitude : {
+        type : Number,
+        required : true,
+    },
+    title : {
+        type : String,
+        requried : true,
+    },
+    description : {
+        type : String,
+        requried: true,
+    },
+    host_id : {
+        type : String,
+        requried : true,
+        ref : 'User',
+    },
+    deadline : {
+        type : Date,
+        required : true,
+    }
+    max_user : {
+        type : Number,
+        required : true,
+        default : 999,
+    },
+    now_user : {
+        type : number,
+        required : true,
+        default: 1,
+    },
+    modified_date : {
+        type : Date,
+    },
+    create_date : {
+        type : Date,
+        default : Date.now,
+    },
+});
+
+module.exports = mongoose.model('Pin',pinSchema);
