00706774fcbcc776502cab87279c79645c38024d
khj745700 <khj745700@naver.com>
2022-01-18 13:42:16 +0900

    리팩토링

--- src/server.js
+++ src/server.js
@@ -6,6 +6,18 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('hello node');
 });
+
+app.set('port', process.env.PORT);
+
+
 dbConnect();
 
 app.listen(process.env.PORT, () => console.log(process.env.PORT,'포트에서 대기중'));
+
+
+app.use((req,res,next)=> {
+    console.log(req);
+    res.writeHead(200);
+    res.end();
+    next();
+});
